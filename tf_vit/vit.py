4e04182b4db7944df5fa2c65462722042e512e33
Arya <Arya.koureshi@gmail.com>
2024-08-28 14:24:24 +0330

    Update vit.py

--- tf_vit/vit.py
+++ tf_vit/vit.py
@@ -3,8 +3,66 @@ from keras import models
 from keras import layers
 from keras import activations
 from .utils import load_imgnet_weights
-from .layers import PatchEmbedding, AddCLSToken, AddPositionEmbedding, TransformerEncoder
+from .layers import PatchEmbedding3D, PatchEmbedding, AddCLSToken, AddPositionEmbedding, TransformerEncoder
 from .config import BASIC_URL, WEIGHTS, VIT_B_CONFIG, VIT_L_CONFIG, WEIGHTS_CONFIG, PRE_TRAINED_WEIGHTS_FNAME
+
+class ViT3D(models.Model):
+    def __init__(
+        self, 
+        volume_size,
+        patch_size,
+        num_classes,
+        hidden_dim,
+        mlp_dim,
+        atten_heads,
+        encoder_depth,
+        dropout_rate,
+        activation,
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.volume_size = volume_size
+        self.patch_size = patch_size
+        self.hidden_dim = hidden_dim
+        self.mlp_dim = mlp_dim
+        self.num_classes = num_classes
+        self.atten_heads = atten_heads
+        self.encoder_depth = encoder_depth
+        self.dropout_rate = dropout_rate
+        self.activation = activations.get(activation)
+
+        self.patch_embedding = PatchEmbedding3D(self.patch_size, self.hidden_dim, name="patch_embedding_3d")
+        self.add_cls_token = AddCLSToken(self.hidden_dim, name="add_cls_token")
+        self.position_embedding = AddPositionEmbedding(name="position_embedding")
+        self.encoder_blocks = [
+            TransformerEncoder(
+                self.mlp_dim,
+                self.atten_heads,
+                self.dropout_rate,
+                name=f"transformer_block_{i}"
+            ) for i in range(self.encoder_depth)
+        ]
+        self.layer_norm = layers.LayerNormalization(epsilon=1e-6, name="layer_norm")
+        self.extract_token = layers.Lambda(lambda x: x[:, 0], name="extract_token")
+        self.mlp_head = layers.Dense(self.num_classes, self.activation, name="mlp_head")
+
+    def call(self, inputs):
+        x = self.patch_embedding(inputs)
+        x = self.add_cls_token(x)
+        x = self.position_embedding(x)
+        for encoder in self.encoder_blocks:
+            x = encoder(x)
+        x = self.layer_norm(x)
+        x = self.extract_token(x)
+        x = self.mlp_head(x)
+        return x
+
+    def model_summary(self, verbose=True):
+        self.build((None, *self.volume_size, 1))  # Ensure the model is built
+        if verbose:
+            self.summary()
+        else:
+            self.summary(line_length=150, positions=[.33, .61, .68, 1.])
 
 
 class ViT(models.Model):
