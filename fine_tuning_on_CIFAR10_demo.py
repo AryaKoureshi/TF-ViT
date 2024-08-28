df1b72b1f39cee3fa89c6a77253cc73c1b3aafaa
Arya <Arya.koureshi@gmail.com>
2024-08-28 09:27:19 +0330

    Update fine_tuning_on_CIFAR10_demo.py

--- fine_tuning_on_CIFAR10_demo.py
+++ fine_tuning_on_CIFAR10_demo.py
@@ -6,29 +6,29 @@ from data import cifar_10_data_gen
 from keras_vit.vit import ViT_B32, ViT
 
 
-#---------------------------------设置参数-------------------------------------
-WEIGHT_CONFIG = "imagenet21k"                      # 配置预训练权重 "imagenet21k" 或者 "imagenet21k+imagenet2012"
-NUM_CLASSES = 10                                   # 输出类别
-DROPOUT_RATE = 0.1                                 # dropout概率
-ACTIVATION = "softmax"                             # 输出头激活函数
+#-----------------------------------------------------------------------------
+WEIGHT_CONFIG = "imagenet21k"            
+NUM_CLASSES = 10                
+DROPOUT_RATE = 0.1                         
+ACTIVATION = "softmax"             
 
-LABEL_SMOOTH = 0.1                                 # 标签平滑系数
-LEARNING_RATE = 1e-3                               # 初始学习率
-BATCH_SIZE = 16                                    # 训练和验证的批大小
-EPOCHS = 100                                       # 训练轮数
-STEPS_PER_EPOCH = 40000//BATCH_SIZE                # 每轮训练的batch数
-VALIDATION_STEPS = 10000//BATCH_SIZE               # 验证的batch数
-TEST_STEPS = 10000//BATCH_SIZE                     # 测试的batch数
+LABEL_SMOOTH = 0.1                        
+LEARNING_RATE = 1e-3                  
+BATCH_SIZE = 16                               
+EPOCHS = 100                                    
+STEPS_PER_EPOCH = 40000//BATCH_SIZE               
+VALIDATION_STEPS = 10000//BATCH_SIZE           
+TEST_STEPS = 10000//BATCH_SIZE                     
 #-----------------------------------------------------------------------------
 
 
-#---------------------------------设置路径-------------------------------------
+#-----------------------------------------------------------------------------
 DATA_PATH = "datasets/cifar-10"
 SAVE_PATH = f"save_models/"
 #-----------------------------------------------------------------------------
 
 
-#---------------------------------搭建模型-------------------------------------
+#-----------------------------------------------------------------------------
 vit = ViT_B32(
     num_classes = NUM_CLASSES, 
     activation = ACTIVATION,
@@ -47,7 +47,7 @@ vit.compile(
 #-----------------------------------------------------------------------------
 
 
-#--------------------------------加载数据集-------------------------------------
+#-----------------------------------------------------------------------------
 train_data_gen, valid_data_gen = cifar_10_data_gen(
     path = DATA_PATH, 
     batch_size = BATCH_SIZE, 
@@ -63,7 +63,7 @@ test_data_gen = cifar_10_data_gen(
 #-----------------------------------------------------------------------------
 
 
-#--------------------------------训练和保存-------------------------------------
+#-----------------------------------------------------------------------------
 best_loss, _ = vit.evaluate(valid_data_gen, steps=VALIDATION_STEPS)
 stop = 0
 for e in range(EPOCHS):
@@ -94,7 +94,7 @@ for e in range(EPOCHS):
 #-----------------------------------------------------------------------------
 
 
-#----------------------------------测试----------------------------------------
+#-----------------------------------------------------------------------------
 vit.evaluate(
     test_data_gen, 
     steps=TEST_STEPS
